ef80fcbafb6be3396276936e12252c3df04c8d99
OlhaDorofiychuk <olha.d@yahoo.com>
2023-03-28 13:17:55 +0100

    routing added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Projects } from './components/Projects';
@@ -9,17 +10,31 @@ function App() {
     <div className="App">
      <header>My Portfolio</header>
      <nav>
-      <ul><li>Home</li>
-      <li>About</li>
-      <li>Projects</li>
+      <ul>
+        <li>
+        <Link to='/'>Home</Link>
+        </li>
+      <li>
+        <Link to='/about'>About</Link>
+        </li>
+      <li>
+      <Link to='/projects'>Projects </Link>
+      </li>
       
       </ul>
      </nav>
-      <Home />
-      <About />
-      <Projects />
+     <main>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/projects' element={<Projects />}/>
+      
+      
+      
+      </Routes>
+      
      
-
+      </main>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ export const Home=()=>{
         <div className="contact_information">
             <span><b> 07877 948 788</b></span>
             <span><b>olha.d@yahoo.com</b></span>
+            <img className="main_photo" src="./IMG_9624.jpg" alt="me"/> 
         </div>
     )
 }
