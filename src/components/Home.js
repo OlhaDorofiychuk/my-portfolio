6f5e0961c749d17ba2357130dccd18bf260b862a
OlhaDorofiychuk <olha.d@yahoo.com>
2023-03-29 20:14:25 +0100

    saving changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 
 export const Home=()=>{
     return(
-        <div>
+        <>
         <div className="contact_information">
             <span><b> 07877 948 788</b></span>
             <span><b>olha.d@yahoo.com</b></span>
+            <button>https://www.linkedin.com/in/olha-dorofiychuk-655353178/</button>
              
         </div>
         <img className="main_photo" src="./IMG_9624.jpg" alt="me"/>
-        </div>
+        </>
     )
 }
