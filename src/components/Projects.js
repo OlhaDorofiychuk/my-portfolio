6c0298caf253e8c1f8ca5a06dd4d17ab1456e8d7
OlhaDorofiychuk <olha.d@yahoo.com>
2023-05-09 08:51:44 +0100

    screenshots added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,22 @@ export const Projects=()=>{
     return(
         <div>
             <ul>
-                <li> First</li>
-                <li>Second</li>
+                <div className="project">
+                    <span className="project-title">Whatch-it</span>
+                    <div className="description">A simple yet aesthetically pleasing app to search and display movies by using TMDB API. User can search movies by name orgenre and change color scheme from neutral grey to bright blue gradient.
+
+</div>
+<img className="screenshot" src="./first.png" alt="screenshot"/> 
+<img className="screenshot" src="./second.png" alt="screenshot"/> 
+
+                    </div>
+                    <div className="project">
+                    <span className="project-title">My ToDo</span>
+                    <div className="description">This project helped me to understand and use React together with Typescript. I have aswell used a great library React_beautifullDND to achive drag and drop functionaity. User can simply move tasks order or move to complited by dragging it.
+
+</div>
+<img className="screenshot" src="./third.png" alt="screenshot"/> 
+                    </div>
                 <li>Third</li>
             </ul>
         </div>
